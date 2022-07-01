92e3412e84655c83e38043b698b3ac3d6478c3ef
Morauszki András <morauszki.andras@gmail.com>
2022-07-01 11:38:57 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import { LogoTwitter } from "react-ionicons";
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "Your quote is loading",
+      author: "",
+    };
+
+    this.getRandomQuote = this.getRandomQuote.bind(this);
+  }
+
+  getRandomQuote = async function () {
+    try {
+      const res = await fetch("https://api.quotable.io/random");
+      const quote = await res.json();
+      this.setState({
+        text: quote.content,
+        author: quote.author,
+      });
+    } catch (error) {
+      console.log(`Something went wrong: {error}`);
+    }
+  };
+
+  componentDidMount() {
+    this.getRandomQuote();
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <h1>Get your quote!</h1>
+        <div id="text">{this.state.text}</div>
+        <div id="author">{this.state.author}</div>
+        <button id="new-quote" onClick={this.getRandomQuote}>
+          New Quote
+        </button>
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote">
+          <LogoTwitter color="#1DA1F2" height="30px" width="30px" />
         </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
